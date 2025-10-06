deffb551817f22ba7dd8b6615c0916fda35d16e2
sonoftesla <kevinopati@gmail.com>
2025-10-06 13:46:18 +0300

    Add missing dependencies

--- src/components/ui/badge.jsx
+++ src/components/ui/badge.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Slot as SlotPrimitive } from "radix-ui"
+import { Slot as SlotPrimitive } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
 
 import { cn } from "@/lib/utils"
